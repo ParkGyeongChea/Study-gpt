9e0a9553c8a515706b7512a44e4ac64794a02d8b
Park Gyeong Chea <qkrrudco123@naver.com>
2026-04-21 17:10:02 +0900

    main 코드 변경(서버 실행 편의성 코드 추가)

---
+++ study-gpt/backend/main.py
@@ -0,0 +1,40 @@
+# main.py
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from api import study_router  # ⭐ router import (경로 중요)
+
+# FastAPI 앱 생성
+app = FastAPI()
+
+#환경변수 로드
+load_dotenv()
+
+
+#CROS 설정 (React 연결)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ⭐ router 연결 (핵심)
+# 라우터 : 사용자의 요청을 받아서 어떤 기능으로 보낼지 결정하는 입구
+app.include_router(study_router.router)
+
+
+
+#로컬 서버 실행 uvicorn backend_api:app --reload =>불편
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app,host="127.0.0.1",port=8000)#FastAPI 서버 실행
+
+
+
+
+
+
+#uvicorn main:app --reload
