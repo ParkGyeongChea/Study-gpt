2dd8f95c8266e5c24cb29406ab35ffade22e3760
Park Gyeong Chea <qkrrudco123@naver.com>
2026-04-24 13:20:44 +0900

    프롬프트 강화 , 라우터 연결코드 추가 작업

--- study-gpt/backend/services/curriculum_service.py
+++ study-gpt/backend/services/curriculum_service.py
@@ -7,9 +7,11 @@
 
 # curriculum_service.py
 
-from backend.services.llm_service import generate_curriculum
-from backend.services.llm_service import analyze_user_input
+# from backend.services.llm_service import generate_curriculum
+# from backend.services.llm_service import analyze_user_input
 
+from services.llm_service import generate_curriculum
+from services.llm_service import analyze_user_input
 
 def start_study_service(message: str): #사용자가 입력한 message
     
@@ -23,11 +25,10 @@ def start_study_service(message: str): #사용자가 입력한 message
     #정리 코드
     
     #2. 그 다음 분석
-    category, level = analyze_user_input(message)
+    category, level, topic = analyze_user_input(message)
     #analyze_user_input 함수 호출 .
     #message를 가지고 분석 함수 실행.
     
-    curriculum = None
     
     # 2. 단계 수(step_count) 결정
     if level == "초급":
@@ -42,19 +43,22 @@ def start_study_service(message: str): #사용자가 입력한 message
     print("DEBUG level:", level)
     print("DEBUG step_count:", step_count)
 
-    curriculum = generate_curriculum(category, level, step_count) #llm_service의 해당 과목의 커리큘럼을 가져와서 저장
+    curriculum = generate_curriculum(category, topic, level, step_count)
+    #llm_service의 해당 과목의 커리큘럼을 가져와서 저장
+    #함수 호출부와 정의부는 개수가 맞아야 함.
     
-    # 4. 못 찾은 경우 (에러 처리) 
-    if category is None: #끝까지 돌았는데도 과목을 못찾음(None). 아래 에러 메시지 출력
-        return {
-            "error": "어떤 과목인지 이해하지 못했어요.",
-            "message": "다시 한번 입력해주세요"
-        }
+    # # 4. 못 찾은 경우 (에러 처리) 
+    # if category is None: #끝까지 돌았는데도 과목을 못찾음(None). 아래 에러 메시지 출력
+    #     return {
+    #         "error": "어떤 과목인지 이해하지 못했어요.",
+    #         "message": "다시 한번 입력해주세요"
+    #     }
 
     # 5. 정상 처리 
     #과목을 찾음. if문 밖, for문에 대한 return
     return {
         "category": category,
+        "topic": topic,
         "curriculum": curriculum
     }
     
